affcbf1283ca4f8ce77645694f2300a15973f8dd
Jacob Sanders <sanders.jms@gmail.com>
2022-01-03 23:27:16 -0700

    wrote script for repeatString and edited repeatString.spec to run all the tests correctly

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = ''
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+        repeatedString += string;
+        }
+        return repeatedString;
+    } else {
+        return 'ERROR';
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
