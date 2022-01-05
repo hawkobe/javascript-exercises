dd15a7ffec67bb2cdbb6d97f03618f4e0f1a6d3e
Jacob Sanders <sanders.jms@gmail.com>
2022-01-04 18:32:53 -0700

    FINALLY finished removeFromArray. Could probably be optimized

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let i = 0; i < args.length; i++) {
+    let arrayIndex = array.indexOf(args[i]);
+    if (arrayIndex >= 0) {
+        array.splice(arrayIndex, 1);
+        }
+    }
 
+    return array;
+    //console.log(args);
 };
 
 // Do not edit below this line
