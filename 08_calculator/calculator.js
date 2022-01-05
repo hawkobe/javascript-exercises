9129722df1c643466418956ab287155028337fb2
Jacob Sanders <sanders.jms@gmail.com>
2022-01-05 16:59:52 -0700

    Finished script for calculator tests!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+    let sum = a + b;
+    return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    let result = a - b;
+    return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    let finalSum = 0;
+    for (let i = 0; i < array.length; i++) {
+        finalSum += array[i];
+    }
+
+    return finalSum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+    let product = array[0];
+    for (let i = 1; i < array.length; i++) {
+        product *= array[i];
+    }
 
+    return product;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+    let product = num ** pow;
+    return product;	
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+    let product = 1;
+
+    for (i = 0; i < factorial; i++) {
+        product *= (factorial - i);
+    }
+    return product;
 };
 
 // Do not edit below this line
