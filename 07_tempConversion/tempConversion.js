65a6d4402606ca4ab90aa906eafeaa1a09ea18d1
Jacob Sanders <sanders.jms@gmail.com>
2022-01-04 20:11:48 -0700

    Finished tempConversion aww yeah!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(degree) {
+    let celsius = (degree - 32) * 5/9;
+    let celsiusOneDec = Number(celsius.toFixed(1));
+    return celsiusOneDec;
 };
 
-const ctof = function() {
-
+const ctof = function(degree) {
+    let fahrenheit = degree * 9/5 + 32;
+    console.log(fahrenheit);
+    let fahrenheitOneDec = Number(fahrenheit.toFixed(1));
+    return fahrenheitOneDec;
 };
 
 // Do not edit below this line
