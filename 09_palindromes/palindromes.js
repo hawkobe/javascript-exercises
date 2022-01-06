069fa9776ff1af698bdca74180c8028e3d60cd08
Jacob Sanders <sanders.jms@gmail.com>
2022-01-05 20:35:18 -0700

    Finished palidromes, albeit a bit verbose

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    //use regex to get string without punctuation
+    const noPunctStr = str.replace(/[^\w]/gi, '');
+    const noPunctStrCap = noPunctStr.toUpperCase();
 
+    let result = ''
+
+    let splitStr = noPunctStrCap.split('');
+    let reverseArray = splitStr.reverse();
+    let joinArray = reverseArray.join('');
+
+    for (let i = 0; i < noPunctStrCap.length -1; i++) {
+        if (joinArray.charAt(i) == noPunctStrCap.charAt(i)) {
+            result = true;
+        } else {
+            result = false;
+            break;
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
