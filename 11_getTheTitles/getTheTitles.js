fbe88e8201839469f48d7fd587eb4152a46f7b5f
Jacob Sanders <sanders.jms@gmail.com>
2022-01-06 00:40:14 -0700

    finished get the titles all by meself with no help!

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
 
+    //I can use the map() method to code this more easily instead!
+
+    let result = []
+
+    for (i = 0; i < array.length; i++) {
+        result[i] = array[i].title;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
