cead86c5dc218406910e3f28c809e049ab8e534c
Jacob Sanders <sanders.jms@gmail.com>
2022-01-06 23:20:31 -0700

    Had to look at solution but understand reduce function better now

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return (oldestAge < currentAge) ? current : oldest;
+    });
 
+
+    function getAge(birthYear, deathYear) {
+        if (!deathYear) {
+            deathYear = new Date().getFullYear();
+        }
+        return deathYear - birthYear;
+    }
+
+  // SOMEHOW USE THE REDUCE FUNCTION. READ ABOUT IT.
+
+  //need to pull info from arrays
+        //probably use people.map((name, yearOfBirth, yearOfDeath) => {
+                //people.yearOfBirth
+                //people.yearOfDeath
+                //people.name
+                //}
+  //need to calculate age of person in array using birth and death years
+        //age = deathYeah - birthYear
+  //need to compare the ages of the different people in the array to find highest
+
+  //will need to use a Date() method for people with no death year
+  //
 };
 
 // Do not edit below this line
